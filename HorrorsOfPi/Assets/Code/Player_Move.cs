0d21cd3c247a244d85e9ecb9bfd44a486739e875
BVisoth <thefracturedowl@gmail.com>
2023-01-30 09:32:39 -0800

    Added Player_Move Script
    Don't have much on it, but it is a start

---
+++ HorrorsOfPi/Assets/Code/Player_Move.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player_Move Script has Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
