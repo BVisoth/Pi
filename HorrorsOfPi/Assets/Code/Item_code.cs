beeb5a86b9574ec6fc80fff5f36a2972e3b0a8e3
BVisoth <thefracturedowl@gmail.com>
2023-03-02 11:22:44 -0800

    Fixed Number Glitch

--- HorrorsOfPi/Assets/Code/Item_code.cs
+++ HorrorsOfPi/Assets/Code/Item_code.cs
@@ -1,28 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Item_code : MonoBehaviour
 {
 
     public int spin;
+    public TextMeshProUGUI CollectedAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CollectedAmount.text = ("Collected Numbers: " + Door_Unlock.amountNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0f, 0f, spin * Time.deltaTime, Space.Self);
+        transform.Rotate(0f, spin * Time.deltaTime, 0f, Space.Self);
     }
 
     public void OnTriggerEnter(Collider player)
     {
         Destroy(gameObject);
         Debug.Log("Collected");
-        Door_Unlock.amountNumber = 0 + 1;
+        Door_Unlock.amountNumber += 1;
+        CollectedAmount.text = ("Collected Numbers: " + Door_Unlock.amountNumber);
     }
 }
